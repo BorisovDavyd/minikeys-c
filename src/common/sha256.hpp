282230c0e4f888a90f85eae9a69e299043f063a8
BorisovDavyd <85416962+BorisovDavyd@users.noreply.github.com>
2025-08-14 13:00:20 +0300

    Fix SHA256 constants for nvcc

--- src/common/sha256.hpp
+++ src/common/sha256.hpp
@@ -25,7 +25,7 @@ HD void sha256_final(SHA256Ctx *ctx, uint8_t hash[32]);
 static HD uint32_t rotr(uint32_t x, uint32_t n){return (x>>n)|(x<<(32-n));}
 
 static HD void sha256_transform(SHA256Ctx *ctx){
-    static const uint32_t k[64] = {
+    const uint32_t k[64] = {
         0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
         0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
         0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
@@ -35,7 +35,7 @@ static HD void sha256_transform(SHA256Ctx *ctx){
         0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,0x391c0cb3,0x4ed8aa4a,0x5b9cca4f,0x682e6ff3,
         0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
     };
-    uint32_t a,b,c,d,e,f,g,h,t1,t2,m[64];
+    uint32_t a,b,c,d,e,f,g,h,m[64];
     for(int i=0;i<16;++i){
         m[i] = (ctx->data[i*4]<<24)|(ctx->data[i*4+1]<<16)|(ctx->data[i*4+2]<<8)|(ctx->data[i*4+3]);
     }
